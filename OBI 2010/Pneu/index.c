f6f14b80592dabd6e70cfed4bd5cae30bb35b946
Allan Silva <hrsallanprogramador@gmail.com>
2025-10-29 13:23:03 -0300

    Estrutura: Adiciona pasta para OBI 2010

---
+++ OBI 2010/Pneu/index.c
@@ -0,0 +1,10 @@
+// Allan Henrique Rosa Silva
+#include <stdio.h>
+
+int main(){
+    int entrada_user,entrada_bomba;
+    scanf("%d",&entrada_user);
+    scanf("%d",&entrada_bomba);
+    printf("%d\n",entrada_user - entrada_bomba);
+    return 0;
+}
